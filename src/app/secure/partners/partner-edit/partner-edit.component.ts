d61e76c9d6424d06680b9d20fcff0ed752e60de8
mili ahmed <ahmedmili76@gmail.com>
2023-03-21 12:45:19 +0100

    [Fix] update image upload

--- src/app/secure/partners/partner-edit/partner-edit.component.ts
+++ src/app/secure/partners/partner-edit/partner-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PartnerService } from 'src/app/services/partner.service';
 
@@ -11,35 +11,61 @@ import { PartnerService } from 'src/app/services/partner.service';
 export class PartnerEditComponent implements OnInit {
   form!: FormGroup;
   id!: number;
+  files: any;
+  formData = new FormData();
   constructor(
     private formBuilder: FormBuilder,
     private partnerService: PartnerService,
     private router: Router,
     private route: ActivatedRoute) { }
 
-    ngOnInit(): void {
-      this.form=this.formBuilder.group({
-        name:'',
-        description:'',
-        email:'',
-        phone:'',
-        password:'',
-        image:'',
-        category:'',
-        openingtime:'', 
-        closingtime:''
-      });
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: ["", [Validators.required]],
+      description: ["", [Validators.required]],
+      email: ["", [Validators.required]],
+      phone: ["", [Validators.required]],
+      password: ["", [Validators.required]],
+      image: ["", [Validators.required]],
+      category: ["", [Validators.required]],
+      openingtime: ["", [Validators.required]],
+      closingtime: ["", [Validators.required]]
+    });
 
-      this.id = this.route.snapshot.params['id'];
+    this.id = this.route.snapshot.params['id'];
 
-      this.partnerService.get(this.id).subscribe(
-        partner => this.form.patchValue(partner)
+    this.partnerService.get(this.id).subscribe(
+      partner =>{
+        this.form.patchValue(partner);
+        console.log(partner);
+      } 
+    );
+  }
+
+  uploadImage(event: any) {
+    this.files = event.target.files[0];
+    console.log(this.files);
+  }
+
+  submit(): void {
+    const formValue = this.form.getRawValue();
+    this.formData.append("name", formValue.name);
+    this.formData.append("description", formValue.description);
+    this.formData.append("email", formValue.email);
+    this.formData.append("phone", formValue.phone);
+    this.formData.append("password", formValue.password);
+    this.formData.append("category", formValue.category);
+    this.formData.append("openingtime", formValue.openingtime);
+    this.formData.append("closingtime", formValue.closingtime);
+    this.formData.append("image", this.files, this.files.name);
+    console.log(formValue);
+    this.partnerService.update(this.id, this.formData)
+      .subscribe((response) => {
+        // if (response.status == 200)
+        console.log(response);
+        this.router.navigate(['/partners']);
+      }
       );
-    }
- 
-    submit(): void {
-      this.partnerService.update(this.id, this.form.getRawValue())
-        .subscribe(() => this.router.navigate(['/partners']));
-    }
+  }
 
 }
--- src/app/services/partner.service.ts
+++ src/app/services/partner.service.ts
@@ -9,10 +9,10 @@ import { Partner } from '../interfaces/partner';
 })
 export class PartnerService {
   private httpOptions = {
-    headers : new HttpHeaders({'Content-Type': 'application/json', 'Authorization':'Bearer ' + localStorage.getItem('token')})
+    headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + localStorage.getItem('token') })
   };
   endpoint = `${environment.api}/partners`;
-  apiUrl=`${environment.api}/searchPartner`;
+  apiUrl = `${environment.api}/searchPartner`;
 
   constructor(private http: HttpClient) { }
 
@@ -22,8 +22,9 @@ export class PartnerService {
 
   create(data: any): Observable<Partner> {
     return this.http.post<Partner>(this.endpoint, data, {
-      headers : new HttpHeaders({
-        'Authorization':'Bearer ' + localStorage.getItem('token')})
+      headers: new HttpHeaders({
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+      })
     });
   }
 
@@ -32,33 +33,33 @@ export class PartnerService {
   }
 
   update(id: number, data: any): Observable<Partner> {
-    return this.http.put<Partner>(`${this.endpoint}/${id}`, data, this.httpOptions);
+    return this.http.post<Partner>(`${environment.api}/update/${id}`, data, this.httpOptions)
   }
 
   delete(id: number): Observable<void> {
     return this.http.delete<void>(`${this.endpoint}/${id}`, this.httpOptions);
   }
 
-  getdetails(id: number): Observable<Partner>{
-    return this.http.get<Partner>(`${this.endpoint}/partnerdetails/${id}`,this.httpOptions);
+  getdetails(id: number): Observable<Partner> {
+    return this.http.get<Partner>(`${this.endpoint}/partnerdetails/${id}`, this.httpOptions);
   }
 
 
   //Search function
-  getPartners(search: string = "", category:string = ""): Observable<Partner[]> {
+  getPartners(search: string = "", category: string = ""): Observable<Partner[]> {
     let apiUrl = this.apiUrl;
     let httpOptions = this.httpOptions;
-  
-      apiUrl += `?search=${search}&category=${category}`;
-    
-    return this.http.get<Partner[]>(apiUrl, httpOptions); 
+
+    apiUrl += `?search=${search}&category=${category}`;
+
+    return this.http.get<Partner[]>(apiUrl, httpOptions);
   }
 
 
 
   getFilteredPartners(category: string): Observable<Partner[]> {
     const url = `${environment.api}/filter?category=${category}`;
-    return this.http.get<Partner[]>(url,this.httpOptions);
+    return this.http.get<Partner[]>(url, this.httpOptions);
   }
 
 
